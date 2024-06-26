251480fedad849cfbc0d805724d772cc196ba40e
Deepak Kumar <92260620+Deeepkk@users.noreply.github.com>
2024-06-26 09:45:09 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_akita_row_call.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_akita_row_call-user \
+    lineage_akita_row_call-userdebug \
+    lineage_akita_row_call-eng
